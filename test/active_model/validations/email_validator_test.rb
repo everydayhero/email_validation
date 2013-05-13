0f6fbfb1a8b03e5710ada817dad111ad384709c6
marty zalega <marty@zalega.me>
2013-05-13 10:09:16 +1000

    Fixed false-positive assertions

--- test/active_model/validations/email_validator_test.rb
+++ test/active_model/validations/email_validator_test.rb
@@ -22,70 +22,63 @@ class ActiveModel::Validations::EmailValidatorTest < ActiveSupport::TestCase
     @model.email = 'chuck@testa'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains no TLD" do
     @model.email = 'chuck@'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains a comma" do
     @model.email = 'chu,ck@testa.com'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when ends with a period" do
     @model.email = 'chu,ck@testa.com.'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains multiple @ symbols" do
     @model.email = 'chu@ck@testa.com'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains no @ symbols" do
     @model.email = 'chucktesta.com'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when value is empty" do
     @model.email = ''
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains spaces" do
     @model.email = 'chu ck@testa.com'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
-  end
-
-  test "add error when contains single-quotes" do
-    @model.email = 'chu\'ck@testa.com'
-    @validator.validate @model
-
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
   test "add error when contains double-quotes" do
     @model.email = 'chu"ck@testa.com'
     @validator.validate @model
 
-    refute_nil @model.errors[:email]
+    refute_empty @model.errors[:email]
   end
 
 end
